ccda25d50cc69f0b236a409f6f6516d8b53954b3
seungok2 <seung1560@gmail.com>
2024-12-17 19:47:57 +0900

    Create Boss Character
    1. Add ignore Asset
    2. Test Map(Nav Mesh)
    3. Create ABP & BlendS (Idle to Run)

---
+++ Source/Team_ProjectA/SOS/private/SOS_BOSS_Character.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Team_ProjectA/SOS/public/SOS_BOSS_Character.h"
+
+// Sets default values
+ASOS_BOSS_Character::ASOS_BOSS_Character()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// 스켈레탈 메쉬 초기화
+	ConstructorHelpers::FObjectFinder<USkeletalMesh> InitMesh(TEXT("/Script/Engine.SkeletalMesh'/Game/SOS/Hideoplast/Meshes/SK_Hideoplast.SK_Hideoplast'"));
+
+	if(InitMesh.Succeeded())
+	{
+		GetMesh()->SetSkeletalMesh(InitMesh.Object);
+
+		//Relate -> 상대적 위치값, 회전값
+		GetMesh()->SetRelativeLocationAndRotation(FVector(0, 0, 0), FRotator(-4, 0, -90));
+		
+	}
+	
+}
+
+// Called when the game starts or when spawned
+void ASOS_BOSS_Character::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASOS_BOSS_Character::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ASOS_BOSS_Character::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
