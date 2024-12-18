2dc8dc4f2479ddc368db5c685ee61eec1bcc5493
seungok2 <seung1560@gmail.com>
2024-12-18 19:59:31 +0900

    MontagePlay Task Test Complete
    1. Make UPROPERTY MONTAGE Play Task
    2. 3 Montage Motion (need Notify)
    3. Testing Behavior Tree

---
+++ Source/Team_ProjectA/SOS/public/SOS_BTTask_DefaultAttack.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "SOS_BTTask_DefaultAttack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEAM_PROJECTA_API USOS_BTTask_DefaultAttack : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+
+	USOS_BTTask_DefaultAttack();
+	
+};
