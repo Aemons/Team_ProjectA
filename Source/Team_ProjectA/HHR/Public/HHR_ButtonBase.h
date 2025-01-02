05ac1b87c0f811c2d66c9a7ab4ce618dc7472493
petemus <93657221+petemus@users.noreply.github.com>
2025-01-02 18:04:45 +0900

    2025-01-02
    UI 작업중

---
+++ Source/Team_ProjectA/HHR/Public/HHR_ButtonBase.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "HHR_ButtonBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEAM_PROJECTA_API UHHR_ButtonBase : public UUserWidget
+{
+	GENERATED_BODY()
+};
